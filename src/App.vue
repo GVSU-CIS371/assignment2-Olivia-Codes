b0c91d3fc380e9da84365f05364b31a84e2ff21f
Olivia-Codes <fostero@mail.gvsu.edu>
2025-10-19 18:11:40 -0400

    Implement radio buttons for beverage selection

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Beverage :isIced="currentTemp === 'Cold'" />
     <ul>
       <li>
+        <h3>Temperature</h3>
         <template v-for="temp in temps" :key="temp">
           <label>
             <input
@@ -16,13 +17,74 @@
           </label>
         </template>
       </li>
+      
+      <li>
+        <h3>Base Beverage</h3>
+        <template v-for="beverage in baseBeverages" :key="beverage">
+          <label>
+            <input
+              type="radio"
+              name="baseBeverage"
+              :id="`b${beverage}`"
+              :value="beverage"
+              v-model="currentBeverage"
+            />
+            {{ beverage }}
+          </label>
+        </template>
+      </li>
+      
+      <li>
+        <h3>Creamer</h3>
+        <template v-for="creamer in creamers" :key="creamer">
+          <label>
+            <input
+              type="radio"
+              name="creamer"
+              :id="`c${creamer}`"
+              :value="creamer"
+              v-model="currentCreamer"
+            />
+            {{ creamer }}
+          </label>
+        </template>
+      </li>
+      
+      <li>
+        <h3>Syrup</h3>
+        <template v-for="syrup in syrups" :key="syrup">
+          <label>
+            <input
+              type="radio"
+              name="syrup"
+              :id="`s${syrup}`"
+              :value="syrup"
+              v-model="currentSyrup"
+            />
+            {{ syrup }}
+          </label>
+        </template>
+      </li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import Beverage from "./components/Beverage.vue";
 import { temps, currentTemp } from "./stores/beverage";
+
+// bev options
+const baseBeverages = ['Coffee', 'Green Tea', 'Black Tea'];
+const currentBeverage = ref('Coffee');
+
+// cream options
+const creamers = ['No Creamer', 'Milk', 'Cream', 'Half & Half'];
+const currentCreamer = ref('No Creamer');
+
+// syrup options
+const syrups = ['No Syrup', 'Vanilla', 'Caramel', 'Hazelnut'];
+const currentSyrup = ref('No Syrup');
 </script>
 
 <style lang="scss">
@@ -36,7 +98,35 @@ html {
   background-color: #6e4228;
   background: linear-gradient(to bottom, #6e4228 0%, #956f5a 100%);
 }
+
 ul {
   list-style: none;
+  padding: 20px;
+  
+  li {
+    margin-bottom: 20px;
+    
+    h3 {
+      color: #fff;
+      margin-bottom: 10px;
+      font-size: 16px;
+    }
+    
+    label {
+      display: block;
+      color: #fff;
+      margin-bottom: 8px;
+      cursor: pointer;
+      
+      input[type="radio"] {
+        margin-right: 8px;
+        cursor: pointer;
+      }
+      
+      &:hover {
+        color: #ffd89b;
+      }
+    }
+  }
 }
 </style>
